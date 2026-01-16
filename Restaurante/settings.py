3934be2ec915d504f85e73956f36a3e8b906ca07
Katherin <katherin.pico2124@utc.edu.ec>
2026-01-16 15:41:43 -0500

    correccion static

--- Restaurante/settings.py
+++ Restaurante/settings.py
@@ -141,6 +141,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Tu carpeta static local (css/js/img)
+STATICFILES_DIRS = [BASE_DIR / "Restaurante" / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
