23f2735f801cddca956f13e0d8d10760044d1bb5
Katherin <katherin.pico2124@utc.edu.ec>
2026-03-05 18:32:00 -0500

    subir 7

--- Aplicacion/gestionPedidos/models.py
+++ Aplicacion/gestionPedidos/models.py
@@ -444,7 +444,7 @@ class ComprobanteCliente(models.Model):
     
     numero_comprobante = models.CharField(max_length=50, unique=True)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
-    imagen = models.ImageField(upload_to='pagos_clientes/')
+    imagen = CloudinaryField('image', folder='pagos_clientes')
     
     estado = models.CharField(max_length=20, choices=ESTADOS, default='pendiente')
     fecha_hora = models.DateTimeField(auto_now_add=True)
